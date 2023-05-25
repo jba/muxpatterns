ea6affc8ad14f641bee042c28ca30921ff446440
Jonathan Amsterdam <jba@google.com>
2023-05-25 17:59:09 -0400

    path patterns use specificity

--- pattern.go
+++ pattern.go
@@ -2,9 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// TODO:
-// - check host validity to catch missing initial slash; e.g. "{x}/a".
-
+// Package muxpatterns is a sample implementation of enhanced
+// http.ServeMux routing patterns.
+// See https://github.com/golang/go/discussions/60227.
+//
+// The API in this package is for experimentation only.
+// It is likely that none of it will be in the proposal.
 package muxpatterns
 
 import (
@@ -32,9 +35,13 @@ var methods = []string{
 
 // A Pattern is something that can be matched against an HTTP request.
 type Pattern struct {
-	method   string
-	host     string
-	segments []segment
+	method string
+	host   string
+	// The representation of a path differs from the surface syntax.
+	// Paths ending in '/' are represented with an anonymous "..." wildcard.
+	// Paths ending in "{$}" are represented with an element ending in '/'.
+	// This makes most algorithms simpler.
+	elements []element
 }
 
 func (p *Pattern) Method() string { return p.method }
@@ -48,7 +55,7 @@ func (p *Pattern) String() string {
 	if p.host != "" {
 		b.WriteString(p.host)
 	}
-	for _, s := range p.segments {
+	for _, s := range p.elements {
 		if !s.wild {
 			b.WriteString(s.s)
 		} else {
@@ -63,25 +70,25 @@ func (p *Pattern) String() string {
 	return b.String()
 }
 
-// A segment is a pattern piece that matches one or more path elements.
-// If wild is false, it matches a sub-path, including slashes.
-// If wild is true and multi is false, it matches a single path element.
-// Otherwise, it matches all remaining path elements.
-type segment struct {
+// An element is a pattern piece that matches one or more path segments.
+// If wild is false, it matches a literal sub-path, including slashes.
+// If wild is true and multi is false, it matches a single path segment.
+// If both wild and multi are true, it matches all remaining path elements.
+type element struct {
 	s     string // literal string or wildcard name
 	wild  bool   // a wildcard
 	multi bool   // wildcard ends in "..."
 }
 
-func (s segment) String() string {
-	if !s.wild {
-		return s.s
+func (e element) String() string {
+	if !e.wild {
+		return e.s
 	}
 	dots := ""
-	if s.multi {
+	if e.multi {
 		dots = "..."
 	}
-	return "{" + s.s + dots + "}"
+	return "{" + e.s + dots + "}"
 }
 
 // Parse parses a string into a Pattern.
@@ -96,14 +103,14 @@ func (s segment) String() string {
 //     a literal or a wildcard of the form "{name}", "{name...}", or "{$}".
 //
 // METHOD, HOST and PATH are all optional; that is, the string can be "/".
-// There must be a single space after METHOD if it is present.
+// If METHOD is present, it must be followed by a single space.
 // Wildcard names must be valid Go identifiers.
-// PATH may end with a '/', unless the final segment is "{$}" or a  "..." wildcard.
+// The "{$}" and "{name...}" wildcard must occur at the end of PATH.
+// PATH may end with a '/'.
 func Parse(s string) (*Pattern, error) {
 	if len(s) == 0 {
 		return nil, errors.New("empty pattern")
 	}
-
 	method, rest, found := strings.Cut(s, " ")
 	if !found {
 		rest = method
@@ -121,6 +128,9 @@ func Parse(s string) (*Pattern, error) {
 	}
 	p.host = rest[:i]
 	rest = rest[i:]
+	if strings.IndexByte(p.host, '{') >= 0 {
+		return nil, errors.New("host contains '{' (missing initial '/'?")
+	}
 	for len(rest) > 0 {
 		// Invariant: rest[0] == '/'
 		i := strings.IndexByte(rest, '{')
@@ -130,7 +140,7 @@ func Parse(s string) (*Pattern, error) {
 		if i > 0 && rest[i-1] != '/' {
 			return nil, errors.New("bad wildcard segment (starts after '/')")
 		}
-		p.segments = append(p.segments, segment{s: rest[:i]})
+		p.elements = append(p.elements, element{s: rest[:i]})
 		rest = rest[i+1:]
 		j := strings.IndexByte(rest, '}')
 		if j < 0 {
@@ -161,17 +171,17 @@ func Parse(s string) (*Pattern, error) {
 		if !isValidWildcardName(name) {
 			return nil, fmt.Errorf("bad wildcard name %q", name)
 		}
-		p.segments = append(p.segments, segment{wild: true, s: name, multi: multi})
+		p.elements = append(p.elements, element{wild: true, s: name, multi: multi})
 	}
 	if len(rest) > 0 {
-		p.segments = append(p.segments, segment{s: rest})
+		p.elements = append(p.elements, element{s: rest})
 		if rest[len(rest)-1] == '/' {
-			p.segments = append(p.segments, segment{wild: true, multi: true})
+			p.elements = append(p.elements, element{wild: true, multi: true})
 		}
 	}
-	for _, s := range p.segments {
-		if !s.wild {
-			if strings.Contains(s.s, "//") {
+	for _, e := range p.elements {
+		if !e.wild {
+			if strings.Contains(e.s, "//") {
 				return nil, errors.New("empty path segment")
 			}
 		}
@@ -210,7 +220,7 @@ func (p *Pattern) Match(method, host, path string) (bool, []string) {
 	if len(path) == 0 || path[0] != '/' {
 		panic("path should start with '/'")
 	}
-	if len(p.segments) == 0 {
+	if len(p.elements) == 0 {
 		panic("pattern has no segments")
 	}
 
@@ -222,13 +232,13 @@ func (p *Pattern) Match(method, host, path string) (bool, []string) {
 	}
 	rest := path
 	var matches []string
-	for _, seg := range p.segments {
-		if seg.multi {
-			if seg.s != "" {
+	for _, el := range p.elements {
+		if el.multi {
+			if el.s != "" {
 				matches = append(matches, rest)
 			}
 			rest = ""
-		} else if seg.wild {
+		} else if el.wild {
 			i := strings.IndexByte(rest, '/')
 			if i < 0 {
 				i = len(rest)
@@ -241,7 +251,7 @@ func (p *Pattern) Match(method, host, path string) (bool, []string) {
 			rest = rest[i:]
 		} else {
 			var found bool
-			rest, found = strings.CutPrefix(rest, seg.s)
+			rest, found = strings.CutPrefix(rest, el.s)
 			if !found {
 				return false, nil
 			}
@@ -253,13 +263,17 @@ func (p *Pattern) Match(method, host, path string) (bool, []string) {
 	return true, matches
 }
 
-// MoreSpecificThan reports whether p1 is more specific than p2, as defined
-// by these rules:
+// HigherPrecedence reports whether p1 has higher precedence than p2.
+// If p1 and p2 both match a request, then p1 will be chosen.
+//
+// Precedence is defined by these rules:
 //
 //  1. Patterns with a host win over patterns without a host.
 //  2. Patterns with a method win over patterns without a method.
-//  3. Prefer patterns whose path component is more specific.
-func (p1 *Pattern) MoreSpecificThan(p2 *Pattern) bool {
+//  3. Patterns whose path is more specific win. One path pattern is more
+//     specific than another if the second matches all the paths of the
+//     first and more.
+func (p1 *Pattern) HigherPrecedence(p2 *Pattern) bool {
 	// 1. Patterns with a host win over patterns without a host.
 	if (p1.host == "") != (p2.host == "") {
 		return p1.host != ""
@@ -268,16 +282,13 @@ func (p1 *Pattern) MoreSpecificThan(p2 *Pattern) bool {
 	if (p1.method == "") != (p2.method == "") {
 		return p1.method != ""
 	}
-	// 3. More specific paths.
+	// 3. More specific paths win.
 	return p1.comparePaths(p2) == moreSpecific
 }
 
-func (p *Pattern) lastSeg() segment {
-	return p.segments[len(p.segments)-1]
-}
-
 // ConflictsWith reports whether p1 conflicts with p2, that is, whether
-// there is a request that both match but where p1 is not more specific than p2.
+// there is a request that both match but where neither is higher precedence
+// than the other.
 func (p1 *Pattern) ConflictsWith(p2 *Pattern) bool {
 	if p1.host != p2.host {
 		// Either one host is empty and the other isn't, in which case the
@@ -290,6 +301,135 @@ func (p1 *Pattern) ConflictsWith(p2 *Pattern) bool {
 		return false
 	}
 	return p1.comparePaths(p2) == overlaps
+}
+
+const (
+	moreSpecific = "moreSpecific"
+	moreGeneral  = "moreGeneral"
+	overlaps     = "overlaps"
+	disjoint     = "disjoint"
+)
+
+// comparePaths classifies the paths of the patterns into one of four
+// groups:
+//
+//	moreGeneral: p1 matches all the paths of p2 and more
+//	moreSpecific: p2 matches all the paths of p1 and more
+//	overlaps: there is a path that both match, but neither is more specific
+//	disjoint: there is no path that both match
+func (p1 *Pattern) comparePaths(p2 *Pattern) string {
+	// Copy the segment slices to make the algorithm simpler.
+	// TODO: avoid the copy; simplify the entire algorithm.
+	segs1 := slices.Clone(p1.elements)
+	segs2 := slices.Clone(p2.elements)
+
+	// Track whether a single (non-multi) wildcard in p1 matched
+	// a literal in p2, and vice versa.
+	// We care about these because if a wildcard matches a literal, then the
+	// pattern with the wildcard can't be more specific than the one with the
+	// literal.
+	wild1MatchedLit2 := false
+	wild2MatchedLit1 := false
+	for len(segs1) > 0 && len(segs2) > 0 {
+		s1 := segs1[0]
+		s2 := segs2[0]
+		if s1.multi && s2.multi {
+			segs1 = segs1[1:]
+			segs2 = segs2[1:]
+			continue
+		}
+		if s1.multi {
+			// p1 matches the rest of p2.
+			// Does that mean it is more general than p2?
+			if !wild2MatchedLit1 {
+				// If p2 didn't have any wildcards that matched literals in p1,
+				// then yes, p1 is more general.
+				return moreGeneral
+			}
+			// Otherwise neither is more general than the other.
+			return overlaps
+		}
+		if s2.multi {
+			// p2 matches the rest of p1.
+			if !wild1MatchedLit2 {
+				return moreSpecific
+			}
+			return overlaps
+		}
+		if s1.wild && s2.wild {
+			// These ordinary wildcards match each other.
+		} else if s1.wild {
+			// p1's ordinary wildcard matches the first segment
+			// of p2's literal, or the rest of it.
+			_, s2.s = splitSegment(s2.s)
+			wild1MatchedLit2 = true
+		} else if s2.wild {
+			_, s1.s = splitSegment(s1.s)
+			wild2MatchedLit1 = true
+		} else {
+			// Two literals.
+			// Consume the common prefix, or fail.
+			i := len(s1.s)
+			if len(s1.s) > len(s2.s) {
+				i = len(s2.s)
+			}
+			if s1.s[:i] != s2.s[:i] {
+				return disjoint
+			}
+			s1.s = s1.s[i:]
+			s2.s = s2.s[i:]
+		}
+		// If this segment is done, advance to the next.
+		if s1.wild || s1.s == "" {
+			segs1 = segs1[1:]
+		} else {
+			segs1[0].s = s1.s // Assign back into slice; s1 is a copy.
+		}
+		if s2.wild || s2.s == "" {
+			segs2 = segs2[1:]
+		} else {
+			segs2[0].s = s2.s
+		}
+	}
+	if len(segs1) == 0 && len(segs2) == 0 {
+		// The patterns matched completely.
+		if wild1MatchedLit2 && !wild2MatchedLit1 {
+			return moreGeneral
+		}
+		if wild2MatchedLit1 && !wild1MatchedLit2 {
+			return moreSpecific
+		}
+		return overlaps
+	}
+	if len(segs1) == 0 {
+		// p2 has at least one more segment.
+		// p1 did not end in a multi.
+		// That means p2's remaining segments must match nothing.
+		// Which means to match, there must be only one segment, a multi.
+		if !segs2[0].multi {
+			return disjoint
+		}
+		if !wild1MatchedLit2 {
+			return moreSpecific
+		}
+		return overlaps
+	}
+	// len(segs2) == 0
+	if !segs1[0].multi {
+		return disjoint
+	}
+	if !wild2MatchedLit1 {
+		return moreGeneral
+	}
+	return overlaps
+}
+
+func splitSegment(path string) (string, string) {
+	i := strings.IndexByte(path, '/')
+	if i < 0 {
+		return path, ""
+	}
+	return path[:i], path[i:]
 }
 
 // A PatternSet is a set of non-conflicting patterns.
@@ -313,13 +453,16 @@ func (s *PatternSet) Register(p *Pattern) error {
 	return nil
 }
 
-// MatchRequest matches an http.Request against the set of patterns, returning
-// the matching pattern and a map from wildcard to matching path segment.
-func (s *PatternSet) MatchRequest(req *http.Request) (*Pattern, map[string]any, error) {
+// MatchRequest calls Match with the request's method, host and path.
+func (s *PatternSet) MatchRequest(req *http.Request) (*Pattern, map[string]string, error) {
 	return s.Match(req.Method, req.URL.Host, req.URL.Path)
 }
 
-func (s *PatternSet) Match(method, host, path string) (*Pattern, map[string]any, error) {
+// Match matches the method, host and path against the patterns in the PatternSet.
+// It returns the highest-precedence matching pattern and a map from wildcard
+// names to matching path segments.
+// Match returns (nil, nil, nil) if there is no matching pattern.
+func (s *PatternSet) Match(method, host, path string) (*Pattern, map[string]string, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	var (
@@ -328,7 +471,7 @@ func (s *PatternSet) Match(method, host, path string) (*Pattern, map[string]any,
 	)
 	for _, p := range s.patterns {
 		if ok, ms := p.Match(method, host, path); ok {
-			if best == nil || p.MoreSpecificThan(best) {
+			if best == nil || p.HigherPrecedence(best) {
 				best = p
 				matches = ms
 			}
@@ -344,11 +487,11 @@ func (s *PatternSet) Match(method, host, path string) (*Pattern, map[string]any,
 // bind returns a map from wildcard names to matched, decoded values.
 // matches is a list of matched substrings in the order that non-empty wildcards
 // appear in the Pattern.
-func (p *Pattern) bind(matches []string) (map[string]any, error) {
-	bindings := map[string]any{}
+func (p *Pattern) bind(matches []string) (map[string]string, error) {
+	bindings := map[string]string{}
 	i := 0
-	for _, seg := range p.segments {
-		if seg.wild && seg.s != "" && seg.s != "$" {
+	for _, seg := range p.elements {
+		if seg.wild && seg.s != "" {
 			bindings[seg.s] = matches[i]
 			i++
 		}
--- pattern_test.go
+++ pattern_test.go
@@ -11,15 +11,15 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	lit := func(name string) segment {
-		return segment{s: name}
+	lit := func(name string) element {
+		return element{s: name}
 	}
 
-	wild := func(name string) segment {
-		return segment{s: name, wild: true}
+	wild := func(name string) element {
+		return element{s: name, wild: true}
 	}
 
-	multi := func(name string) segment {
+	multi := func(name string) element {
 		s := wild(name)
 		s.multi = true
 		return s
@@ -29,58 +29,58 @@ func TestParse(t *testing.T) {
 		in   string
 		want Pattern
 	}{
-		{"/", Pattern{segments: []segment{lit("/"), multi("")}}},
-		{"/a", Pattern{segments: []segment{lit("/a")}}},
+		{"/", Pattern{elements: []element{lit("/"), multi("")}}},
+		{"/a", Pattern{elements: []element{lit("/a")}}},
 		{
 			"/a/",
-			Pattern{segments: []segment{lit("/a/"), multi("")}},
+			Pattern{elements: []element{lit("/a/"), multi("")}},
 		},
-		{"/path/to/something", Pattern{segments: []segment{
+		{"/path/to/something", Pattern{elements: []element{
 			lit("/path/to/something"),
 		}}},
 		{
 			"/{w1}/lit/{w2}",
 			Pattern{
-				segments: []segment{lit("/"), wild("w1"), lit("/lit/"), wild("w2")},
+				elements: []element{lit("/"), wild("w1"), lit("/lit/"), wild("w2")},
 			},
 		},
 		{
 			"/{w1}/lit/{w2}/",
 			Pattern{
-				segments: []segment{lit("/"), wild("w1"), lit("/lit/"), wild("w2"), lit("/"), multi("")},
+				elements: []element{lit("/"), wild("w1"), lit("/lit/"), wild("w2"), lit("/"), multi("")},
 			},
 		},
 		{
 			"example.com/",
-			Pattern{host: "example.com", segments: []segment{lit("/"), multi("")}},
+			Pattern{host: "example.com", elements: []element{lit("/"), multi("")}},
 		},
 		{
 			"GET /",
-			Pattern{method: "GET", segments: []segment{lit("/"), multi("")}},
+			Pattern{method: "GET", elements: []element{lit("/"), multi("")}},
 		},
 		{
 			"POST example.com/foo/{w}",
 			Pattern{
 				method:   "POST",
 				host:     "example.com",
-				segments: []segment{lit("/foo/"), wild("w")},
+				elements: []element{lit("/foo/"), wild("w")},
 			},
 		},
 		{
 			"/{$}",
-			Pattern{segments: []segment{lit("/")}},
+			Pattern{elements: []element{lit("/")}},
 		},
 		{
 			"DELETE example.com/{$}",
-			Pattern{method: "DELETE", host: "example.com", segments: []segment{lit("/")}},
+			Pattern{method: "DELETE", host: "example.com", elements: []element{lit("/")}},
 		},
 		{
 			"/foo/{$}",
-			Pattern{segments: []segment{lit("/foo/")}},
+			Pattern{elements: []element{lit("/foo/")}},
 		},
 		{
 			"/{a}/foo/{rest...}",
-			Pattern{segments: []segment{lit("/"), wild("a"), lit("/foo/"), multi("rest")}},
+			Pattern{elements: []element{lit("/"), wild("a"), lit("/foo/"), multi("rest")}},
 		},
 	} {
 		got, err := Parse(test.in)
@@ -114,6 +114,7 @@ func TestParseError(t *testing.T) {
 		{"/{$}/x", "{$} not at end"},
 		{"/{a...}/", "not at end"},
 		{"/{a...}/x", "not at end"},
+		{"{a}/b", "missing initial '/'"},
 	} {
 		_, err := Parse(test.in)
 		if err == nil || !strings.Contains(err.Error(), test.contains) {
@@ -123,7 +124,7 @@ func TestParseError(t *testing.T) {
 }
 
 func (p1 *Pattern) equal(p2 *Pattern) bool {
-	return p1.method == p2.method && p1.host == p2.host && slices.Equal(p1.segments, p2.segments)
+	return p1.method == p2.method && p1.host == p2.host && slices.Equal(p1.elements, p2.elements)
 }
 
 func TestMatch(t *testing.T) {
@@ -276,7 +277,184 @@ func TestMatch(t *testing.T) {
 	}
 }
 
-func TestMoreSpecificThan(t *testing.T) {
+func TestComparePaths(t *testing.T) {
+	for _, test := range []struct {
+		p1, p2 string
+		want   string
+	}{
+		// TODO: verify we hit all these case below in our systematic list.
+		{"/a/{$}", "/a", disjoint},
+		{"/", "/a", moreGeneral},
+		{"/{x...}", "/a", moreGeneral},
+		{"/", "/{x}", moreGeneral},
+		{"/", "/{$}", moreGeneral},
+		{"/a/b/{x...}", "/a/b/c/d/{y...}", moreGeneral},
+		{"/a", "/a", overlaps},
+		{"/a", "/ab", disjoint},
+		{"/{x}", "/{x}", overlaps},
+		{"/a/{x...}", "/a/b/{x...}", moreGeneral},
+		{"/a/{$}", "/a/b/{x...}", disjoint},
+		{"/a/b/{$}", "/a/b/{x...}", moreSpecific},
+		{"/a/{x}/b/{y...}", "/{x}/c/{y...}", overlaps},
+		{"/a/{x}/b/", "/{x}/c/{y...}", overlaps},
+		{"/a/{x}/b/{$}", "/{x}/c/{y...}", overlaps},
+		{"/a/{x...}", "/b/{y...}", disjoint},
+		{"/a/{x...}", "/a/{y...}", overlaps},
+		{"/a/{z}/{x...}", "/a/b/{y...}", moreGeneral},
+		{"/a/{z}/{x...}", "/{z}/b/{y...}", overlaps},
+		{"/a/{x...}", "/a/{x}/{y...}", moreGeneral},
+
+		// A non-final pattern segment can have one of two values: literal or
+		// single wildcard. A final pattern segment can have one of 5: empty
+		// (trailing slash), literal, dollar, single wildcard, or multi
+		// wildcard. Trailing slash and multi wildcard are the same.
+
+		// A literal should be more specific than anything it overlaps, except itself.
+		{"/a", "/a", overlaps},
+		{"/a", "/b", disjoint},
+		{"/a", "/", moreSpecific},
+		{"/a", "/{$}", disjoint},
+		{"/a", "/{x}", moreSpecific},
+		{"/a", "/{x...}", moreSpecific},
+
+		// Adding a segment doesn't change that.
+		{"/b/a", "/b/a", overlaps},
+		{"/b/a", "/b/b", disjoint},
+		{"/b/a", "/b/", moreSpecific},
+		{"/b/a", "/b/{$}", disjoint},
+		{"/b/a", "/b/{x}", moreSpecific},
+		{"/b/a", "/b/{x...}", moreSpecific},
+		{"/{z}/a", "/{z}/a", overlaps},
+		{"/{z}/a", "/{z}/b", disjoint},
+		{"/{z}/a", "/{z}/", moreSpecific},
+		{"/{z}/a", "/{z}/{$}", disjoint},
+		{"/{z}/a", "/{z}/{x}", moreSpecific},
+		{"/{z}/a", "/{z}/{x...}", moreSpecific},
+
+		// Single wildcard on left.
+		{"/{z}", "/a", moreGeneral},
+		{"/{z}", "/a/b", disjoint},
+		{"/{z}", "/{$}", disjoint},
+		{"/{z}", "/{x}", overlaps},
+		{"/{z}", "/", moreSpecific},
+		{"/{z}", "/{x...}", moreSpecific},
+		{"/b/{z}", "/b/a", moreGeneral},
+		{"/b/{z}", "/b/a/b", disjoint},
+		{"/b/{z}", "/b/{$}", disjoint},
+		{"/b/{z}", "/b/{x}", overlaps},
+		{"/b/{z}", "/b/", moreSpecific},
+		{"/b/{z}", "/b/{x...}", moreSpecific},
+
+		// Trailing slash on left.
+		{"/", "/a", moreGeneral},
+		{"/", "/a/b", moreGeneral},
+		{"/", "/{$}", moreGeneral},
+		{"/", "/{x}", moreGeneral},
+		{"/", "/", overlaps},
+		{"/", "/{x...}", overlaps},
+
+		{"/b/", "/b/a", moreGeneral},
+		{"/b/", "/b/a/b", moreGeneral},
+		{"/b/", "/b/{$}", moreGeneral},
+		{"/b/", "/b/{x}", moreGeneral},
+		{"/b/", "/b/", overlaps},
+		{"/b/", "/b/{x...}", overlaps},
+
+		{"/{z}/", "/{z}/a", moreGeneral},
+		{"/{z}/", "/{z}/a/b", moreGeneral},
+		{"/{z}/", "/{z}/{$}", moreGeneral},
+		{"/{z}/", "/{z}/{x}", moreGeneral},
+		{"/{z}/", "/{z}/", overlaps},
+		{"/{z}/", "/a/", moreGeneral},
+		{"/{z}/", "/{z}/{x...}", overlaps},
+		{"/{z}/", "/a/{x...}", moreGeneral},
+		{"/a/{z}/", "/{z}/a/", overlaps},
+
+		// Multi wildcard on left.
+		{"/{m...}", "/a", moreGeneral},
+		{"/{m...}", "/a/b", moreGeneral},
+		{"/{m...}", "/{$}", moreGeneral},
+		{"/{m...}", "/{x}", moreGeneral},
+		{"/{m...}", "/", overlaps},
+		{"/{m...}", "/{x...}", overlaps},
+
+		{"/b/{m...}", "/b/a", moreGeneral},
+		{"/b/{m...}", "/b/a/b", moreGeneral},
+		{"/b/{m...}", "/b/{$}", moreGeneral},
+		{"/b/{m...}", "/b/{x}", moreGeneral},
+		{"/b/{m...}", "/b/", overlaps},
+		{"/b/{m...}", "/b/{x...}", overlaps},
+
+		{"/{z}/{m...}", "/{z}/a", moreGeneral},
+		{"/{z}/{m...}", "/{z}/a/b", moreGeneral},
+		{"/{z}/{m...}", "/{z}/{$}", moreGeneral},
+		{"/{z}/{m...}", "/{z}/{x}", moreGeneral},
+		{"/{z}/{m...}", "/{z}/", overlaps},
+		{"/{z}/{m...}", "/a/", moreGeneral},
+		{"/{z}/{m...}", "/{z}/{x...}", overlaps},
+		{"/{z}/{m...}", "/a/{x...}", moreGeneral},
+		{"/a/{z}/{m...}", "/{z}/a/", overlaps},
+
+		// Dollar on left.
+		{"/{$}", "/a", disjoint},
+		{"/{$}", "/a/b", disjoint},
+		{"/{$}", "/{$}", overlaps},
+		{"/{$}", "/{x}", disjoint},
+		{"/{$}", "/", moreSpecific},
+		{"/{$}", "/{x...}", moreSpecific},
+
+		{"/b/{$}", "/b/a", disjoint},
+		{"/b/{$}", "/b/a/b", disjoint},
+		{"/b/{$}", "/b/{$}", overlaps},
+		{"/b/{$}", "/b/{x}", disjoint},
+		{"/b/{$}", "/b/", moreSpecific},
+		{"/b/{$}", "/b/{x...}", moreSpecific},
+
+		{"/{z}/{$}", "/{z}/a", disjoint},
+		{"/{z}/{$}", "/{z}/a/b", disjoint},
+		{"/{z}/{$}", "/{z}/{$}", overlaps},
+		{"/{z}/{$}", "/{z}/{x}", disjoint},
+		{"/{z}/{$}", "/{z}/", moreSpecific},
+		{"/{z}/{$}", "/a/", overlaps},
+		{"/{z}/{$}", "/a/{x...}", overlaps},
+		{"/{z}/{$}", "/{z}/{x...}", moreSpecific},
+		{"/a/{z}/{$}", "/{z}/a/", overlaps},
+	} {
+		pat1, err := Parse(test.p1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		pat2, err := Parse(test.p2)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if g := pat1.comparePaths(pat1); g != overlaps {
+			t.Errorf("%s does not match itself; got %s", pat1, g)
+		}
+		if g := pat2.comparePaths(pat2); g != overlaps {
+			t.Errorf("%s does not match itself; got %s", pat2, g)
+		}
+		got := pat1.comparePaths(pat2)
+		if got != test.want {
+			t.Errorf("%s vs %s: got %s, want %s", test.p1, test.p2, got, test.want)
+		}
+		var want2 string
+		switch test.want {
+		case moreSpecific:
+			want2 = moreGeneral
+		case moreGeneral:
+			want2 = moreSpecific
+		default:
+			want2 = test.want
+		}
+		got2 := pat2.comparePaths(pat1)
+		if got2 != want2 {
+			t.Errorf("%s vs %s: got %s, want %s", test.p2, test.p1, got2, want2)
+		}
+	}
+}
+
+func TestHigherPrecedence(t *testing.T) {
 	for _, test := range []struct {
 		p1, p2 string
 		want   bool
@@ -326,9 +504,9 @@ func TestMoreSpecificThan(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		got := pat1.MoreSpecificThan(pat2)
+		got := pat1.HigherPrecedence(pat2)
 		if got != test.want {
-			t.Errorf("%q.MoreSpecificThan(%q) = %t, want %t",
+			t.Errorf("%q.HigherPrecedence(%q) = %t, want %t",
 				test.p1, test.p2, got, test.want)
 		}
 	}
@@ -385,8 +563,10 @@ func TestPatternSetMatch(t *testing.T) {
 		"POST /item/{user}",
 		"/item/{user}",
 		"/item/{user}/{id}",
+		"/item/{user}/new",
 		"/item/{$}",
 		"POST alt.com/item/{userp}",
+		"/path/{p...}",
 	} {
 		pat, err := Parse(p)
 		if err != nil {
@@ -399,15 +579,25 @@ func TestPatternSetMatch(t *testing.T) {
 
 	for _, test := range []struct {
 		method, host, path string
-		want               map[string]any // nil -> no match, empty -> match
+		want               map[string]string // nil -> no match, empty -> match
 	}{
-		{"GET", "", "/item/jba", map[string]any{"user": "jba"}},
-		{"POST", "", "/item/jba/17", map[string]any{"user": "jba", "id": "17"}},
-		{"GET", "", "/item/", map[string]any{}},
-		{"GET", "", "/item/jba/17/line2", map[string]any{}},
-		{"POST", "alt.com", "/item/jba", map[string]any{"userp": "jba"}},
-		{"GET", "alt.com", "/item/jba", map[string]any{"user": "jba"}},
-		{"GET", "", "/item", nil},
+		{"GET", "", "/item/jba",
+			map[string]string{"user": "jba"}},
+		{"POST", "", "/item/jba/17",
+			map[string]string{"user": "jba", "id": "17"}},
+		{"GET", "", "/item/jba/new",
+			map[string]string{"user": "jba"}},
+		{"GET", "", "/item/",
+			map[string]string{}}, // matches with no bindings
+		{"GET", "", "/item/jba/17/line2",
+			map[string]string{}}, // matches with no bindings
+		{"POST", "alt.com", "/item/jba",
+			map[string]string{"userp": "jba"}},
+		{"GET", "alt.com", "/item/jba",
+			map[string]string{"user": "jba"}},
+		{"GET", "", "/item", nil}, // does not match
+		{"GET", "", "/path/to/file",
+			map[string]string{"p": "to/file"}},
 	} {
 		if test.host == "" {
 			test.host = "example.com"
